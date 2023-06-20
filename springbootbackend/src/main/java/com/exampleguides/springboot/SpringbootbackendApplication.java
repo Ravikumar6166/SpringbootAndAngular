27f8c1f545eb02de1b88fa6de59554ef2254e313
Ravikumar6166 <ravi6166raj@gmail.com>
2023-06-20 12:05:48 +0530

    SpringbootwithAngular

---
+++ springbootbackend/src/main/java/com/exampleguides/springboot/SpringbootbackendApplication.java
@@ -0,0 +1,13 @@
+package com.exampleguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootbackendApplication.class, args);
+	}
+
+}
---
+++ springbootbackend/src/main/java/com/exampleguides/springboot/model/Employee.java
@@ -0,0 +1,82 @@
+package com.exampleguides.springboot.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	@Column(name = "email_id")
+	private String emailId;
+	
+	@Column(name = "designation")
+	private String designation;
+
+	public Employee(int id, String firstName, String lastName, String emailId, String designation) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+		this.designation = designation;
+	}
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getDesignation() {
+	return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+}
